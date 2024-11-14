ac4136fdf834ba237f10ca5284b712601bff152b
Julien Dusabe <j.dusabe@alustudent.com>
2024-11-14 01:32:14 -0800

    creates a second table

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- create a table in a database
+CREATE TABLE IF NOT EXISTS second_table(
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+INSERT INTO second_table(id, name, score)
+              VALUES (1, John, 10),
+		     (2, Alexi, 3),
+		     (3, Bob, 14),
+		     (4, George, 8);
