bab3d99fbb32fca85833ef8cc46ec307185593e4
Julien Dusabe <j.dusabe@alustudent.com>
2025-06-27 16:55:25 -0700

    1-square

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module defines a Square class with a private size attribute."""
+
+class Square:
+    """Represents a square with a private size."""
+
+    def __init__(self, size):
+        """Initialize a new Square instance.
+
+        Args:
+            size: The size of the square (no validation for now).
+        """
+        self.__size = size
