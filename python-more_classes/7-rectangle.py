95a163be5868302f18f4ec7658171c9909f43c38
Julien Dusabe <j.dusabe@alustudent.com>
2025-06-27 18:56:15 -0700

    7-rec

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -57,7 +57,8 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         sym = str(self.print_symbol)
-        return "\n".join([sym * self.__width for _ in range(self.__height)])
+        return "\n".join([sym * self.__width
+            for _ in range(self.__height)])
 
     def __repr__(self):
         """Return a string to recreate the instance with eval()."""
