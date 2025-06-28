22214bd5b1ec06da5ca2008cc8046869ed16a9c9
Julien Dusabe <j.dusabe@alustudent.com>
2025-06-27 18:28:35 -0700

    3-rectangle

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -51,6 +51,8 @@ class Rectangle:
         """Return a string representation of the rectangle using '#' characters."""
         if self.__width == 0 or self.__height == 0:
             return ""
-        lines = ["#" * self.__width
-                for _ in range(self.__height)]
+                lines = []
+        for _ in range(self.__height):
+            lines.append("#" * self.__width)
         return "\n".join(lines)
+
