b4be800981a0f200395271e19bda9e46b22df55d
Julien DUSABE <j.dusabe@alustudent.com>
2025-07-12 18:23:03 -0500

    12-pascal_triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Function that returns Pascal's Triangle of n."""
+
+
+def pascal_triangle(n):
+    """Return a list of lists representing Pascal’s Triangle of n."""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # First row
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]  # Start with 1
+
+        # Fill the middle values
+        for j in range(1, len(prev_row)):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+
+        new_row.append(1)  # End with 1
+        triangle.append(new_row)
+
+    return triangle
+
