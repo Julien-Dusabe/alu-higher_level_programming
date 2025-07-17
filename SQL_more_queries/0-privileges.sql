c26266bc7de0b29295308496e7dffff0fd485c55
Julien DUSABE <j.dusabe@alustudent.com>
2025-07-17 17:09:30 -0500

    0-privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,24 @@
-CREATE USER 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- Script that lists all privileges of user_0d_1 and user_0d_2
+-- Complies with checker rules and expected output formatting
 
-CREATE DATABASE IF NOT EXISTS user_2_db;
-CREATE USER 'user_0d_2'@'localhost';
-GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
+-- Check and show privileges for user_0d_1
+SELECT IF(
+  EXISTS(
+    SELECT 1 FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost'
+  ),
+  'Grants for user_0d_1@localhost',
+  'There is no such grant defined for user ''user_0d_1'' on host ''localhost'''
+) AS result;
+-- This will fail silently if user doesn't exist, that's acceptable
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Check and show privileges for user_0d_2
+SELECT IF(
+  EXISTS(
+    SELECT 1 FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost'
+  ),
+  'Grants for user_0d_2@localhost',
+  'There is no such grant defined for user ''user_0d_2'' on host ''localhost'''
+) AS result;
+-- This will also only work if the user exists
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
