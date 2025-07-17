0051806d2dfcb7a60401e09a2aa7a8112eabb456
Julien DUSABE <j.dusabe@alustudent.com>
2025-07-17 17:08:11 -0500

    0-privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,6 @@
--- For user_0d_1
-SELECT 'Grants for user_0d_1@localhost';
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+CREATE USER 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
--- For user_0d_2
-SELECT 'Grants for user_0d_2@localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+CREATE DATABASE IF NOT EXISTS user_2_db;
+CREATE USER 'user_0d_2'@'localhost';
+GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
