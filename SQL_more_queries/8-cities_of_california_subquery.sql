b54f19015d6e66e8d62a5185902976ce3a7b7cf4
Julien DUSABE <j.dusabe@alustudent.com>
2025-07-17 17:49:58 -0500

    8-cities_of_california

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,4 +3,6 @@ INSERT IGNORE INTO states (id, name) VALUES (1, 'California');
 
 -- Insert 3 California cities (if needed)
 INSERT INTO cities (state_id, name)
-VALUES (1, 'Los Angeles'), (1, 'San Francisco'), (1, 'San Jose');
+VALUES (1, 'Los Angeles'), 
+	(1, 'San Francisco'), 
+	(1, 'San Jose');
