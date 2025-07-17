d8347ef79b88084b7d148afa4c6ff1b3bda1dd90
Julien DUSABE <j.dusabe@alustudent.com>
2025-07-17 17:12:07 -0500

    0-privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,24 +1,5 @@
--- Script that lists all privileges of user_0d_1 and user_0d_2
--- Complies with checker rules and expected output formatting
-
--- Check and show privileges for user_0d_1
-SELECT IF(
-  EXISTS(
-    SELECT 1 FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost'
-  ),
-  'Grants for user_0d_1@localhost',
-  'There is no such grant defined for user ''user_0d_1'' on host ''localhost'''
-) AS result;
--- This will fail silently if user doesn't exist, that's acceptable
+-- List privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- Check and show privileges for user_0d_2
-SELECT IF(
-  EXISTS(
-    SELECT 1 FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost'
-  ),
-  'Grants for user_0d_2@localhost',
-  'There is no such grant defined for user ''user_0d_2'' on host ''localhost'''
-) AS result;
--- This will also only work if the user exists
+-- List privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
