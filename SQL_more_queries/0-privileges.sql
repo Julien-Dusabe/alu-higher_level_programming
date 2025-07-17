783185bd52cb61b008a9021849a862a45aec351c
Julien DUSABE <j.dusabe@alustudent.com>
2025-07-17 16:52:48 -0500

    0-privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,11 @@
--- Trying
+-- Create user_0d_1 as root-like user (if not exists)
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
+-- Create user_0d_2 with limited privileges on user_2_db
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
+
+-- Show grants
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
-
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
