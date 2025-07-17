6a9916f307defef0ea62b465a6c2764381924501
Julien DUSABE <j.dusabe@alustudent.com>
2025-07-17 17:17:46 -0500

    0-privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,7 @@
--- List privileges for user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Lets see
 
--- List privileges for user_0d_2
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+Grants for user_0d_1@localhost
+GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost` ...
+
+Grants for user_0d_2@localhost
+GRANT SELECT, INSERT ON `user_2_db`.* TO `user_0d_2`@`localhost`
