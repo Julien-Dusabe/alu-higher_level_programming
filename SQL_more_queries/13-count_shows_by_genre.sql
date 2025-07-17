88fd53bbab05bd7a198a1adde38238447b6ad67a
Julien DUSABE <j.dusabe@alustudent.com>
2025-07-17 18:32:22 -0500

    13-count

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
 -- oyaah
-SELECT genres.name AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.name
-HAVING number_of_shows > 0
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
