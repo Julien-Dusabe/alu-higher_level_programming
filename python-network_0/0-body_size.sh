2fa6a303b138820ec8f152c75830ce03fa84189f
Julien DUSABE <j.dusabe@alustudent.com>
2025-07-19 15:41:31 -0500

    0-body

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# 0-body_size.sh
-
-# Takes a URL as first argument
-url="$1"
-
-# Use curl with -s for silent and -w '%{size_download}' to output body size
-# We discard the actual body by redirecting output to /dev/null
-curl -s -w "%{size_download}" "$url" -o /dev/null
+curl -s -w "%{size_download}" "$1" -o /dev/null
 echo
