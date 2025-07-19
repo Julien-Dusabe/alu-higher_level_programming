c8556b5a2885e97059d32d0018645208444f4f3e
Julien DUSABE <j.dusabe@alustudent.com>
2025-07-19 15:51:40 -0500

    0-body

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays the size (in bytes) of the body of the response from a given URL
-curl -s -w "%{size_download}\n" "http://$1" -o /dev/null
+curl -s -w "%{size_download}\n" "http://$1/" -o /dev/null
